8aee2c81aaeddda251d79b4ed3ee53e01c6cd599
Arthur <pennywennyweed@gmail.com>
2024-08-23 14:01:30 +0300

    add functions onchange btc input, onclick plus/minus btc buttons

--- veiw.js
+++ veiw.js
@@ -3,79 +3,25 @@ const elButtonSetUSD = document.querySelector('.field_button > input')
 elButtonSetUSD.onclick = onClickSetUSD
 
 function onClickSetUSD() {
-  renderInputTextBTC()
+  renderInputTextBTC(1000)
 }
 
-function renderInputTextBTC(num) {
+function renderInputTextBTC(value) {
   const elInputText = document.querySelector('.row > input[ctrl="btc"]')
-  elInputText.value = num
+  elInputText.value = value
 }
 
+function renderProgress(value, max) {
+  const listProgresses = document.querySelectorAll('progress[ctrl="btc"]')
+  listProgresses.forEach(progress => {
+    progress.max = max
+    progress.value = value
+  })
+}
 
+function renderBTC(value, max) {
+  renderInputTextBTC(value)
+  renderProgress(value, max)
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function renderUSDRange() {
-//   const elInputTotalBalance = document.querySelector('#total_balance')
-//   const elRangeInputs = document.querySelectorAll('input[type="range"]')
-
-//   elRangeInputs.forEach(input => {
-//     input.max = elInputTotalBalance.value
-//   })
-// }
-
-// const elRangeInputs = document.querySelectorAll('input[type="range"]')
-// const elProgressElements = document.querySelectorAll('progress')
-// const elInputUSDValues = document.querySelectorAll('.row > input')
-
-// function onRangeChange(e) {
-//   elProgressElements.value = e.target.value
-//   elInputUSDValues.textContent = e.target.value
-// }
-
-// elRangeInputs.forEach(rangeInput => {
-//   rangeInput.onchange = onRangeChange
-// })
+function renderBTCMinusButton() {}
