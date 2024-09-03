3c55c1846be51eec2614eed3391f7398ffbf432b
YouROCK <113207809+yourock1988@users.noreply.github.com>
2024-09-03 23:25:41 +0300

    refactoring view

--- veiw.js
+++ veiw.js
@@ -1,17 +1,11 @@
-const elInputTotalBalance = document.querySelector('#total_balance')
 const elButtonSetUSD = document.querySelector('.field_button > input')
-elButtonSetUSD.onclick = onClickSetTotalUSD
 
-function onClickSetTotalUSD() {
-  const elInputTotalBalance = document.querySelector('#total_balance')
-  const total = parseInt(elInputTotalBalance.value)
-  handleSetTotalUSD(total)
-}
+const listInputTexts = document.querySelectorAll('.shifter [type="text"]')
 
-const elInputTextBTC = document.querySelector('[type="text"][ctrl="btc"]')
-const elInputTextETH = document.querySelector('[type="text"][ctrl="etc"]')
-const elInputTextLTC = document.querySelector('[type="text"][ctrl="ltc"]')
-const elInputTextDOGE = document.querySelector('[type="text"][ctrl="doge"]')
+// const elInputTextBTC = document.querySelector('[type="text"][ctrl="btc"]')
+// const elInputTextETH = document.querySelector('[type="text"][ctrl="etc"]')
+// const elInputTextLTC = document.querySelector('[type="text"][ctrl="ltc"]')
+// const elInputTextDOGE = document.querySelector('[type="text"][ctrl="doge"]')
 const elButtonMinusBTC = document.querySelector('[ctrl="btc"][action="minus"]')
 const elButtonMinusETH = document.querySelector('[ctrl="etc"][action="minus"]')
 const elButtonMinusLTC = document.querySelector('[ctrl="ltc"][action="minus"]')
@@ -23,23 +17,19 @@ const elButtonPlusETH = document.querySelector('[ctrl="etc"][action="plus"]')
 const elButtonPlusLTC = document.querySelector('[ctrl="ltc"][action="plus"]')
 const elButtonPlusDOGE = document.querySelector('[ctrl="doge"][action="plus"]')
 
-const elRangeInputBTC = document.querySelector(
-  'input[type="range"][ctrl="btc"]'
-)
-const elRangeInputETH = document.querySelector(
-  'input[type="range"][ctrl="etc"]'
-)
-const elRangeInputLTC = document.querySelector(
-  'input[type="range"][ctrl="ltc"]'
-)
-const elRangeInputDOGE = document.querySelector(
-  'input[type="range"][ctrl="doge"]'
-)
+const elRangeInputBTC = document.querySelector('[type="range"][ctrl="btc"]')
+const elRangeInputETH = document.querySelector('[type="range"][ctrl="etc"]')
+const elRangeInputLTC = document.querySelector('[type="range"][ctrl="ltc"]')
+const elRangeInputDOGE = document.querySelector('[type="range"][ctrl="doge"]')
 
-elInputTextBTC.oninput = onInputInputText
-elInputTextETH.oninput = onInputInputText
-elInputTextLTC.oninput = onInputInputText
-elInputTextDOGE.oninput = onInputInputText
+listInputTexts.forEach(elInputText => {
+  elInputText.oninput = onInputInputText
+})
+
+// elInputTextBTC.oninput = onInputInputText
+// elInputTextETH.oninput = onInputInputText
+// elInputTextLTC.oninput = onInputInputText
+// elInputTextDOGE.oninput = onInputInputText
 elRangeInputBTC.oninput = onChangeRangeInput
 elRangeInputETH.oninput = onChangeRangeInput
 elRangeInputLTC.oninput = onChangeRangeInput
@@ -52,33 +42,18 @@ elButtonMinusBTC.onclick = onClickMinusButton
 elButtonMinusETH.onclick = onClickMinusButton
 elButtonMinusLTC.onclick = onClickMinusButton
 elButtonMinusDOGE.onclick = onClickMinusButton
+elButtonSetUSD.onclick = onClickSetTotalUSD
 
-function renderCrypto(value, max, crypto) {
-  renderInputText(value, crypto)
-  renderInputRange(value, max, crypto)
-  renderProgress(value, max, crypto)
+function onClickSetTotalUSD() {
+  const elInputTotalBalance = document.querySelector('#total_balance')
+  const total = parseInt(elInputTotalBalance.value)
+  handleSetTotalUSD(total)
 }
+
 function onInputInputText(e) {
-  const crypto = e.target.id.substring(5, 9)
+  const crypto = e.target.getAttribute('ctrl')
   const value = parseInt(e.target.value)
   handleInputInputUsd(value, crypto)
-}
-function renderInputText(value, crypto) {
-  const elInputText = document.querySelector(`[type="text"][ctrl="${crypto}"]`)
-  elInputText.value = value
-}
-function renderProgress(value, max, crypto) {
-  const listProgresses = document.querySelectorAll(`progress[ctrl="${crypto}"]`)
-  listProgresses.forEach(progress => {
-    progress.max = max
-    progress.value = value
-  })
-}
-
-function renderInputRange(value, max, crypto) {
-  const elRange = document.querySelector(`[type="range"][ctrl="${crypto}"]`)
-  elRange.max = max
-  elRange.value = value
 }
 
 function onClickMinusButton(e) {
@@ -95,4 +70,29 @@ function onChangeRangeInput(e) {
   const crypto = e.target.getAttribute('ctrl')
   const value = parseInt(e.target.value)
   handleRangeInput(value, crypto)
+}
+
+function renderCrypto(value, max, crypto) {
+  renderInputText(value, crypto)
+  renderInputRange(value, max, crypto)
+  renderProgress(value, max, crypto)
+}
+
+function renderInputText(value, crypto) {
+  const elInputText = document.querySelector(`[type="text"][ctrl="${crypto}"]`)
+  elInputText.value = value
+}
+
+function renderProgress(value, max, crypto) {
+  const listProgresses = document.querySelectorAll(`progress[ctrl="${crypto}"]`)
+  listProgresses.forEach(progress => {
+    progress.max = max
+    progress.value = value
+  })
+}
+
+function renderInputRange(value, max, crypto) {
+  const elRange = document.querySelector(`[type="range"][ctrl="${crypto}"]`)
+  elRange.max = max
+  elRange.value = value
 }
