caaac5dd6ac64513a019366750b110ac9e617474
YouROCK <113207809+yourock1988@users.noreply.github.com>
2024-09-19 23:56:21 +0300

    rename api.js identifiers

--- api.js
+++ api.js
@@ -4,22 +4,30 @@ const API_URL = 'https://min-api.cryptocompare.com/data'
 const API_KEY =
   '60f2a30ac76737b31217e6df583ef2a685dde6cd3f08837eb3d5b60f63f97076'
 
-async function fetchCryptoCur(crypto) {
-  const API = `${API_URL}/price?fsym=${crypto}&tsyms=USD&api_key=${API_KEY}`
-  const resp = await fetch(API)
-  const cur = await resp.json()
-  return cur
+const urlCryptoPrice = crypto =>
+  `${API_URL}/price?fsym=${crypto}&tsyms=USD&api_key=${API_KEY}`
+
+const urlCryptoNamesList = `${API_URL}/blockchain/list?api_key=${API_KEY}`
+
+async function sendRequest(url) {
+  const resp = await fetch(url)
+  const data = await resp.json()
+  return data
 }
 
-async function fetchCryptoNames() {
-  const resp = await fetch(`${API_URL}/blockchain/list?api_key=${API_KEY}`)
-  const json = await resp.json()
-  console.log(json.Data)
-  return Object.keys(json.Data).map(d => d.toLowerCase())
+async function getCryptoPrice(crypto) {
+  const url = urlCryptoPrice(crypto)
+  const data = await sendRequest(url)
+  return data
+}
+
+async function getAvailableCryptos() {
+  const data = await sendRequest(urlCryptoNamesList)
+  return Object.keys(data.Data).map(d => d.toLowerCase())
 }
 
 async function initCryptoDropdown() {
-  const cryptos = await fetchCryptoNames()
+  const cryptos = await getAvailableCryptos()
   renderCryptoDatalist(cryptos)
   const dropdown = document.getElementById('newshifter')
   dropdown.addEventListener('input', handleCryptoDataList)
